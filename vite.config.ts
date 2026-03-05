e9aebcbcb4b1697cfb4c810c85d0801ccf7f5387
Sri Krishna Sai Kota <srikrishnasaikota@gmail.com>
2026-03-05 12:16:29 -0500

    Configure Vite deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/portifolio-website/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
